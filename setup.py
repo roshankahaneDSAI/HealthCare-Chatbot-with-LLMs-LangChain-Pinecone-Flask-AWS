e0ea418fc1ea5e327bf91a595da5969291d715d0
roshankahaneDSAI <roshankahane09@gmail.com>
2025-11-25 18:08:46 +0530

    Rag chain experimented successfully and created modular components

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="medical_chatbot",
+    name="healthcare_chatbot",
     version="0.1.0",
-    author="Boktiar Ahmed Bappy",
-    author_email="entbappy73@gmail.com",
+    author="roshankahaneDSAI",
+    author_email="roshankahane09@gmail.com",
     packages=find_packages(),
     install_requires=[]
 )
